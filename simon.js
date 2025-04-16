7bb6db6c817476aada3f011da686d0769b1dcb63
DEV KUMAR RAY <devsisodiya931@gmail.com>
2025-04-16 12:30:35 +0530

    simon.js

--- simon.js
+++ simon.js
@@ -5,14 +5,28 @@ let btns = ["yellow", "red", "purple", "green"];
 
 let started = false;
 let level = 0;
+let highScore = 0;
 
 let h2 = document.querySelector("h2");
 
+// Sound objects
+let sounds = {
+    red: new Audio("sounds/red.mp3"),
+    yellow: new Audio("sounds/yellow.mp3"),
+    green: new Audio("sounds/green.mp3"),
+    purple: new Audio("sounds/purple.mp3"),
+    wrong: new Audio("sounds/wrong.mp3")
+};
+
 document.addEventListener("keypress", function () {
     if (!started) {
-        console.log("game is started");
         started = true;
-        levelUp();
+        level = 0;
+        gameSeq = [];
+        h2.innerText = "Get Ready!";
+        setTimeout(() => {
+            levelUp();
+        }, 1000);
     }
 });
 
@@ -30,19 +44,33 @@ function userFlash(btn) {
     }, 250);
 }
 
+function playSound(color) {
+    if (sounds[color]) {
+        sounds[color].currentTime = 0;
+        sounds[color].play();
+    }
+}
+
 function levelUp() {
     userSeq = [];
     level++;
-    h2.innerText = `Level ${level}`;
+    animateLevelChange(level);
 
     let randIdx = Math.floor(Math.random() * 4);
-    let randColor = btns[randIdx]; 
+    let randColor = btns[randIdx];
     let randBtn = document.querySelector(`.${randColor}`);
 
     gameSeq.push(randColor);
-    console.log(gameSeq);
-
+    playSound(randColor);
     gameFlash(randBtn);
+}
+
+function animateLevelChange(level) {
+    h2.innerText = `Level ${level}`;
+    h2.classList.add("level-up");
+    setTimeout(() => {
+        h2.classList.remove("level-up");
+    }, 300);
 }
 
 function checkAns(idx) {
@@ -51,21 +79,26 @@ function checkAns(idx) {
             setTimeout(levelUp, 1000);
         }
     } else {
-        h2.innerHTML = `Game over! Your score was <b>${level}</b> <br> Press any key to start.`;
+        playSound("wrong");
         document.querySelector("body").style.backgroundColor = "red";
         setTimeout(() => {
             document.querySelector("body").style.backgroundColor = "white";
         }, 150);
+
+        if (level > highScore) highScore = level;
+
+        h2.innerHTML = `Game Over! Score: <b>${level}</b> | High Score: <b>${highScore}</b><br>Press any key to restart.`;
         reset();
     }
 }
 
 function btnPress() {
     let btn = this;
-    userFlash(btn);
-
     let userColor = btn.getAttribute("id");
+
     userSeq.push(userColor);
+    playSound(userColor);
+    userFlash(btn);
 
     checkAns(userSeq.length - 1);
 }
